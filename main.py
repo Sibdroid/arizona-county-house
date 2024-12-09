72fa6c1726b7d5afb9693303e2ca9f8de9cbb4c3
Sibdroid <ryabovglebrg@gmail.com>
2024-12-09 13:06:44 +0100

    feat: set a time delay, it works! just have to fix the name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
+import time
 
 
 def soup_to_counties(soup: BeautifulSoup) -> list[BeautifulSoup]:
@@ -13,18 +14,28 @@ def general_to_info(general: BeautifulSoup) -> (str, int):
     name = name.find("span").text
     total_vote_count = total_vote_count.text
     total_vote_count = total_vote_count.replace(" votes", "").replace(",", "")
+    total_vote_count = total_vote_count.replace(u'\xa0', u'')
     return name, int(total_vote_count)
 
 
 def county_to_data(county: BeautifulSoup) -> (str, list[int]):
     general, votes, *other = county.find_all("div")
+    name, total = general_to_info(general)
+    dem = 0
+    rep = 0
     table = votes.find("div").find("table")
     candidates = table.find("tbody")
     for candidate in candidates.find_all("tr"):
         name, party, votes, *other = candidate.find_all("td")
         party = party.find("div").text
-        print(votes.find("div"))
         votes = votes.find("div").find("span").text
+        votes = int(votes.replace(" ", "").replace(u'\xa0', u''))
+        if party == "D":
+            dem += votes
+        elif party == "R":
+            rep += votes
+    return name, dem, rep, total
+            
 
 
 def main():
@@ -33,10 +44,11 @@ def main():
     driver = webdriver.Chrome()
     driver.get(url)
     driver.execute_script('document.title')
+    time.sleep(3)
     html = driver.page_source
     soup = BeautifulSoup(html)
     counties = soup_to_counties(soup)
-    county_to_data(counties[0])
+    print(county_to_data(counties[0]))
 
 
 if __name__ == "__main__":
