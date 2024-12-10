6a81cb3897f4d79e874030e1b6683024d497d3f2
Sibdroid <ryabovglebrg@gmail.com>
2024-12-10 13:59:38 +0100

    chore: add documentation, fix name, add main()

--- results_analysis.py
+++ results_analysis.py
@@ -3,24 +3,37 @@ import pandas as pd
 
 
 def list_to_unique_list(list_: list) -> list:
+    """Removes duplicates from a list.
+
+    Args:
+        list_ (list): a list of values.
+
+    Returns:
+        An ordered list with no duplicates.
+    """
     return list(dict.fromkeys(list_))
 
 
-def clear_county(name: str) -> str:
+def _clear_county(name: str) -> str:
     for i in ["D", "R", "Total"]:
         name = name.replace(i, "")
     return name.strip()
 
 
-with open(r"results.json") as file:
-    data = json.load(file)
-counties = list_to_unique_list([clear_county(i) for i in data.keys()])
-percentages = []
-for county in counties:
-     d = round(data[f"{county} D"]/data[f"{county} Total"]*100, 2)
-     r = round(data[f"{county} R"]/data[f"{county} Total"]*100, 2)
-     percentages += [[d, r]]
-df = pd.DataFrame(percentages, index=counties, columns=["D", "R"])
-print(df)
+def main():
+    with open(r"results.json") as file:
+        data = json.load(file)
+    counties = list_to_unique_list([_clear_county(i) for i in data.keys()])
+    percentages = []
+    for county in counties:
+         d = round(data[f"{county} D"]/data[f"{county} Total"]*100, 2)
+         r = round(data[f"{county} R"]/data[f"{county} Total"]*100, 2)
+         percentages += [[d, r]]
+    df = pd.DataFrame(percentages, index=counties, columns=["D", "R"])
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
      
      
